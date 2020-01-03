612300b90164903d2c96990eb596c203424e8b9e
Refayat Haque <refayathaque@gmail.com>
2020-01-03 18:48:25 +0800

    Jan 3 1848

--- src/JottoGuessedWords.js
+++ src/JottoGuessedWords.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const JottoGuessedWords = ({ guessedWord }) => {
+const JottoGuessedWords = ({ guessedWords }) => {
+  let content
+  if (guessedWords.length === 0) {
+    content = <span data-test='guess-instructions'>Try to guess the secret words!</span>
+  } else {
+    const guessWordsRows = guessedWords.map((word, index) => (
+      <tr data-test='guessed-word' key={index }>
+        <td>{word.guessedWord}</td>
+        <td>{word.letterMatchCount}</td>
+      </tr>
+    ))
+
+    content = (
+      <div data-test="guessed-words">
+        <h3>Guessed words</h3>
+        <table>
+          <thead>
+            <tr><th>Guess</th><th>Matching Letters</th></tr>
+          </thead>
+          <tbody>
+            { guessWordsRows }
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+
   return (
     <div data-test='jotto-guessed-words-component'>
-      
+      {content}
     </div>
   )
 }
--- src/JottoGuessedWords.test.js
+++ src/JottoGuessedWords.test.js
@@ -15,10 +15,61 @@ const setup = (props = {}) => {
 test('JottoGuessedWords renders without crashing and specifically renders itself and not another component', () => {
   const wrapper = setup()
   expect(wrapper).toBeTruthy();
-  const jottoGuessedWordsComponent = wrapper.find("[data-test='jotto-guessed-words-component']")
+  const jottoGuessedWordsComponent = findByTestAttr(wrapper, 'jotto-guessed-words-component')
   expect(jottoGuessedWordsComponent.length).toBe(1);
 });
 
 test('does not throw warning with expected props', () => {
   checkProps(JottoGuessedWords, defaultProps);
 });
+
+describe('if there are no words guessed', () => {
+  let wrapper
+  // ^ initialized here so that all tests can access the wrapper, which will be initialized and assigned the within the scope of the `beforeEach` function below
+
+  beforeEach(() => {
+     wrapper = setup({ guessedWords: [] })
+  })
+  // ^ runs and assigns value to `wrapper` before each test
+
+  test('renders without error', () => {
+    const component = findByTestAttr(wrapper, 'jotto-guessed-words-component')
+    expect(component.length).toBe(1);
+  })
+  // ^ same as first test in this file
+
+  test('renders instructions to guess a word', () => {
+    const instructions = findByTestAttr(wrapper, 'guess-instructions')
+    expect(instructions.text().length).not.toBe(0)
+  })
+})
+
+describe('if there are words guessed', () => {
+  const guessedWords = [
+    { guessedWord: 'train', letterMatchCount: 3 },
+    { guessedWord: 'agile', letterMatchCount: 1 },
+    { guessedWord: 'party', letterMatchCount: 5 },
+    { guessedWord: 'adopt', letterMatchCount: 2 }
+  ];
+
+  let wrapper
+
+  beforeEach(() => {
+     wrapper = setup({ guessedWords })
+  })
+
+  test('renders without error', () => {
+    const component = findByTestAttr(wrapper, 'jotto-guessed-words-component')
+    expect(component.length).toBe(1);
+  })
+
+  test('renders "guessed words" section', () => {
+    const guessedWordsSectionNode = findByTestAttr(wrapper, 'guessed-words')
+    expect(guessedWordsSectionNode.length).toBe(1);
+  })
+
+  test('displays correct number of guessed words', () => {
+    const guessedWordNodes = findByTestAttr(wrapper, 'guessed-word')
+    expect(guessedWordNodes.length).toBe(guessedWords.length);
+  })
+})
