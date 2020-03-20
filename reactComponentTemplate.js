d97b1d3f69e1f318234fb8e324fad8d3fd4be766
Refayat Haque <refayathaque@users.noreply.github.com>
2020-03-20 11:46:32 -0400

    Create reactComponentTemplate.js

---
+++ reactComponentTemplate.js
@@ -0,0 +1,23 @@
+// react
+// react-bootstrap
+// other libs/imports
+// images
+// components
+// action reducers
+
+const ComponentName = () => {
+
+  const renderBody = () => {
+    return (
+      'helloWorld'
+    )
+  }
+
+  return (
+    <Container>
+      {renderBody()}
+    </Container>
+  )
+}
+
+export default ComponentName;
