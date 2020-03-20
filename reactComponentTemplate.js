22711acff341d26a66c5021af25132946f352fb6
Refayat Haque <refayathaque@users.noreply.github.com>
2020-03-20 11:49:22 -0400

    Update reactComponentTemplate.js

--- reactComponentTemplate.js
+++ reactComponentTemplate.js
@@ -1,5 +1,13 @@
 // react
+import React, { useState, useEffect } from 'react';
 // react-bootstrap
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Image from 'react-bootstrap/Image';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import Alert from 'react-bootstrap/Alert';
+import Button from 'react-bootstrap/Button';
 // other libs/imports
 // images
 // components
